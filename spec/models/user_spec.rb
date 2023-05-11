5ff5aaff81ab13691cb45404fdb313c1de682231
Alexander Saderdinov <al.saderdinov@gmail.com>
2023-05-11 22:52:56 +0300

    comments added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to have_many(:posts).dependent(:destroy) }
+  it { is_expected.to have_many(:comments).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
